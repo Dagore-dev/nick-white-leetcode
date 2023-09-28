9ac531c28fb84c92818d31173d3b63c52b93fd65
Dagore <david.gomez.redondo.dev@gmail.com>
2023-09-28 08:53:43 +0200

    Pascal's Triangle

---
+++ PascalsTriangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nick_white_leetcode;
+
+public class PascalsTriangle
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        IList<IList<int>> result = new List<IList<int>>();
+        result.Add(new List<int> { 1 });
+
+        IList<int> previous;
+        List<int> current;
+
+        for (int i = 1; i < numRows; i++)
+        {
+            previous = result[i - 1];
+            current = new List<int> { 1 };
+
+            for (int j = 1; j < i; j++)
+            {
+                current.Add(previous[j - 1] + previous[j]);
+            }
+
+            current.Add(1);
+            result.Add(current);
+        }
+
+        return result;
+    }
+}
