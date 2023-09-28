70200d193a792ab55a5305377b41ee07fb6da25e
Dagore <david.gomez.redondo.dev@gmail.com>
2023-09-28 16:33:01 +0200

    Contains Duplicate I

---
+++ ContainsDuplicate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nick_white_leetcode;
+
+public class ContainsDuplicate
+{
+    public bool ContainsDuplicateI(int[] nums)
+    {
+        HashSet<int> seen = new();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!seen.Add(nums[i]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
